d49cc88036fe7ffa5c29663f9a50df53825b5cdb
Mateusz Koncikowski <mateusz.koncikowski@dka.io>
2020-04-14 17:11:03 +0200

    Add working address list example with InfiniteScroll component

--- src/components/AddressList.js
+++ src/components/AddressList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
@@ -6,8 +6,7 @@ import ListItemText from '@material-ui/core/ListItemText';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Avatar from '@material-ui/core/Avatar';
 import ImageIcon from '@material-ui/icons/Image';
-import WorkIcon from '@material-ui/icons/Work';
-import BeachAccessIcon from '@material-ui/icons/BeachAccess';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -19,32 +18,45 @@ const useStyles = makeStyles((theme) => ({
 function AddressList() {
   const classes = useStyles();
 
+  const [hasMore, setHasMore] = useState(true);
+  const [items, setItems] = useState(Array.from({ length: 40 }));
+
+  function fetchMoreData() {
+    if (items.length >= 100) {
+      setHasMore(false);
+      return;
+    }
+    setTimeout(() => {
+      setItems(items.concat(Array.from({ length: 20 })));
+    }, 500);
+  }
+
   return (
     <List className={classes.root}>
-      <ListItem>
-        <ListItemAvatar>
-          <Avatar>
-            <ImageIcon />
-          </Avatar>
-        </ListItemAvatar>
-        <ListItemText primary="Photos" secondary="Jan 9, 2014" />
-      </ListItem>
-      <ListItem>
-        <ListItemAvatar>
-          <Avatar>
-            <WorkIcon />
-          </Avatar>
-        </ListItemAvatar>
-        <ListItemText primary="Work" secondary="Jan 7, 2014" />
-      </ListItem>
-      <ListItem>
-        <ListItemAvatar>
-          <Avatar>
-            <BeachAccessIcon />
-          </Avatar>
-        </ListItemAvatar>
-        <ListItemText primary="Vacation" secondary="July 20, 2014" />
-      </ListItem>
+      <InfiniteScroll
+        dataLength={items.length}
+        next={fetchMoreData}
+        hasMore={hasMore}
+        loader={<h4>Loading...</h4>}
+        endMessage={
+          <p style={{ textAlign: 'center' }}>
+            <b>Yay! You have seen it all</b>
+          </p>
+        }
+      >
+        {items.map((i, index) => {
+          return (
+            <ListItem key={index}>
+              <ListItemAvatar>
+                <Avatar>
+                  <ImageIcon />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary={i} secondary="Jan 9, 2014" />
+            </ListItem>
+          );
+        })}
+      </InfiniteScroll>
     </List>
   );
 }
